7820abbbb052f8656e0adec419e369e2186c0f15
LanceFontanilla <lancefontanilla@gmail.com>
2023-10-11 17:10:06 -0600

    created table

--- allSpice/dbSetup.sql
+++ allSpice/dbSetup.sql
@@ -1,8 +1,70 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name varchar(255) COMMENT 'User Name',
-  email varchar(255) COMMENT 'User Email',
-  picture varchar(255) COMMENT 'User Picture'
-) default charset utf8 COMMENT '';
+CREATE TABLE
+    IF NOT EXISTS accounts(
+        id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+        name varchar(255) COMMENT 'User Name',
+        email varchar(255) COMMENT 'User Email',
+        picture varchar(255) COMMENT 'User Picture'
+    ) default charset utf8 COMMENT '';
+
+CREATE TABLE
+    IF NOT EXISTS recipes(
+        id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+        title VARCHAR(100) NOT NULL,
+        instructions VARCHAR(5000) NOT NULL,
+        coverImg VARCHAR(500) NOT NULL,
+        category VARCHAR(100) NOT NULL,
+        creatorId VARCHAR(255) NOT NULL,
+        FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+    ) default charset utf8 COMMENT '';
+
+INSERT INTO
+    recipes(
+        title,
+        instructions,
+        coverImg,
+        category,
+        creatorId
+    )
+VALUES (
+        'Garlic Bread',
+        'Generously butter French bread, add chopped garlic and garlic powder. Add parmesan cheese.  Bake in oven for 20 mins at 350 degrees F',
+        'https://images.unsplash.com/photo-1619535860434-ba1d8fa12536?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=896&q=80',
+        'Side Dish',
+        '65271aec327a5ab76abb0c6c'
+    );
+
+INSERT INTO
+    recipes(
+        title,
+        instructions,
+        coverImg,
+        category,
+        creatorId
+    )
+VALUES (
+        'Cheese Bread',
+        'Generously butter French bread, add salt and pepper. Add parmesan cheese, mozarella cheese and cheddar cheese. Bake in oven for 20 mins at 350 degrees F',
+        'https://images.unsplash.com/photo-1619531040576-f9416740661b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y2hlZXNlJTIwYnJlYWR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=400&q=60',
+        'Side Dish',
+        '65271aec327a5ab76abb0c6c'
+    );
+
+INSERT INTO
+    recipes(
+        title,
+        instructions,
+        coverImg,
+        category,
+        creatorId
+    )
+VALUES (
+        'Mac and Cheese',
+        'Boil noodles, add heavy cream, cheese, spices and melt, add the Noodles to the cheese',
+        'https://images.unsplash.com/photo-1612892010343-983bfd063dc5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWFjJTIwYW5kJTIwY2hlZXNlfGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60',
+        'Main Dish Dinner',
+        '65271aec327a5ab76abb0c6c'
+    )
